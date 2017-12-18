8089b78a19ebf4e3b265c8e9f95dbff966611eef
Agadagluglu <github@punch.eu.com>
2017-12-18 17:21:13 +0100

    Building infrastructure.tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,10 +1,18 @@
+# Global vars declaration
+variable "region" {}
+
+# Instance vars declaration
+
+
 # Define instance
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+resource "aws_instance" "node" {
+  ami           = "ami-8fd760f6"
   instance_type = "t2.micro"
+  key_name      = "LT-APU-Ubuntu"
+  tags          = "[Name=Node]"
 }
 
 # Assing elastic IP
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
+  instance = "${aws_instance.node.id}"
 }
