c1bca3b1fbc443347c11e20376d869604e432bcc
Agadagluglu <github@punch.eu.com>
2017-12-19 10:27:57 +0100

    Adding Ansible post conf command in tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -4,15 +4,25 @@ variable "region" {}
 # Instance vars declaration
 
 
-# Define instance
+# Instance provisioning
 resource "aws_instance" "node" {
   ami           = "ami-8fd760f6"
   instance_type = "t2.micro"
   key_name      = "LT-APU-Ubuntu"
-  tags          = "[Name=Node]"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "dev-node"
+  }
+    
+    # This is where we configure the instance with ansible-playbook
+    provisioner "local-exec" {
+        command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key /home/$USER/.ssh/id-rsa.pub -i '${aws_instance.node.public_ip},' playbook.yml"
+    }
+
 }
 
 # Assing elastic IP
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.node.id}"
-}
+#resource "aws_eip" "ip" { 
+#  instance = "${aws_instance.node.id}"
+#}
