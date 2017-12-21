7e33ef1628165c1433fb35e06e22a50d476ce8d9
Agadagluglu <github@punch.eu.com>
2017-12-21 14:55:23 +0100

    Cleaning up API script

--- app/app/api/endpoints/search.py
+++ app/app/api/endpoints/search.py
@@ -6,21 +6,26 @@ from ...main import app
 
 @app.route('/search/', methods=['POST'])
 def route_search():
-    url = "https://api-adresse.data.gouv.fr/search/"
 #    params = { 'q': '11 impasse canart', 'hn': '11', 'st': 'Impasse Canart', 'pc': '75012', 'ct': 'Paris', 'limit': '1' }
 
     if request.method == 'POST':
+        # Gather parameters of the user's request
         params = request.data.get('data', '')
+
+        # Request address API with user's params
+        url = "https://api-adresse.data.gouv.fr/search/" 
         r = requests.get(url, params)
         rq = r.json()
 
+        # Check the address
         for items in rq['features']:
             if (    items['properties']['housenumber'].lower() == params['hn'].lower()
-                and items['properties']['street'].lower() == params['st'].lower()
-                and items['properties']['postcode'].lower() == params['pc'].lower()
-                and items['properties']['city'].lower() == params['ct'].lower()
+                and items['properties']['street'].lower()      == params['st'].lower()
+                and items['properties']['postcode'].lower()    == params['pc'].lower()
+                and items['properties']['city'].lower()        == params['ct'].lower()
             ):
                 result = "Valid"
             else:
                 result = "Invalid"
+            # Return json    
             return jsonify(result)
