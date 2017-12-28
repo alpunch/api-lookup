463e1fb2d2320c590f96758f3609f9db45fe8bb4
Agadagluglu <github@punch.eu.com>
2017-12-28 17:04:40 +0100

    Refactoring search.py

--- app/app/api/endpoints/search.py
+++ app/app/api/endpoints/search.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask import request
 
 from ...main import app
-
+from ..utils import query_addr
 
 @app.route('/search/', methods=['POST'])
 def route_search():
@@ -10,22 +10,6 @@ def route_search():
 
     if request.method == 'POST':
         # Gather parameters of the user's request
-        params = request.data.get('data', '')
-
-        # Request address API with user's params
-        url = "https://api-adresse.data.gouv.fr/search/" 
-        r = requests.get(url, params)
-        rq = r.json()
-
-        # Check the address
-        for items in rq['features']:
-            if (    items['properties']['housenumber'].lower() == params['hn'].lower()
-                and items['properties']['street'].lower()      == params['st'].lower()
-                and items['properties']['postcode'].lower()    == params['pc'].lower()
-                and items['properties']['city'].lower()        == params['ct'].lower()
-            ):
-                result = "Valid"
-            else:
-                result = "Invalid"
-            # Return json    
-            return jsonify(result)
+        params = request.get_json()
+        # Query the address api using our moudule
+        return query_addr(params)
