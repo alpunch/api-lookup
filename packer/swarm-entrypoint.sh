3a8b8ce6c9e416c6689e712a0fc5d711bf906546
Agadagluglu <github@punch.eu.com>
2018-01-03 14:29:37 +0100

    Adding key name to lauch config

---
+++ packer/swarm-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check wheter a swarm has already been set up
+# Check for /opt/swarm.json
+
+## if file exist, check if node is part of swarm
+### if yes exit
+### else join swarm
+
+## else generate swarm and create /opt/swarm.json
+
+
